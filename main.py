b584cc08fd5af63385b7af670df326b256e72943
Brendon Mota <bmota@its.jnj.com>
2021-03-11 20:48:51 -0300

    Creating way to record system audio

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import subprocess
+import os
 import speech_recognition as sr
 
+directory = 'sounds'
+filepath = directory + '/output.wav'
 r = sr.Recognizer()
 
 def getTextFromAudio(filepath):
-	with sr.AudioFile(filepath) as source:
-		audio_data = r.record(source)
-		text = r.recognize_google(audio_data)
-		return text
+  with sr.AudioFile(filepath) as source:
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data)
+    return text
 
-print(getTextFromAudio("sounds/output.wav"))
+if not os.path.exists(directory):
+  os.makedirs(directory)
+
+subprocess.call("main.exe " + filepath)
+text = getTextFromAudio(filepath)
+print(text)
