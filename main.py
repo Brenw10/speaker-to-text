db130295f17e622efbf8d102d7db596cb8ede7f5
Brendon Mota <bmota@its.jnj.com>
2021-03-11 12:54:06 -0300

    change function naming

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import speech_recognition as sr
 
-def getAudioText(filepath):
+def getTextFromAudio(filepath):
     with sr.AudioFile(filepath) as source:
         r = sr.Recognizer()
         audio_data = r.record(source)
         text = r.recognize_google(audio_data)
         return text
 
-print(getAudioText("sounds/test.wav"))
+print(getTextFromAudio("sounds/test.wav"))
