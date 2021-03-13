4df15c0bc16cb785fe6a4a5d9efe77caef643556
Brendon Mota <bmota@its.jnj.com>
2021-03-13 17:48:47 -0300

    Creating sounds folder

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "audio.c"
 
 int main() {
+    mkdir("sounds");
     start_system_record("sounds/output.wav");
     sleep(3);
     stop_system_record();
