e3155b4f6d2676334953c4b2ca8babc03b1b16ac
Brendon Mota <bmota@its.jnj.com>
2021-03-14 15:09:48 -0300

    Creating file dynamically

--- src/main.c
+++ src/main.c
@@ -1,13 +1,25 @@
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "audio.c"
+#include "string.c"
 
 int main() {
-    mkdir("sounds");
-    start_system_record("sounds/output.wav");
+    char record_path[] = "record/";
+    char sounds_path[] = "sounds/";
+    char extension[] = ".wav";
+
+    mkdir(record_path);
+    mkdir(sounds_path);
+
+    char* index_str = convert_int_to_string(1);
+    char record_filepath[strlen(record_path) + strlen(index_str) + strlen(extension)];
+    strcpy(record_filepath, record_path);
+    strcat(record_filepath, index_str);
+    strcat(record_filepath, extension);
+
+    start_system_record(record_filepath);
     sleep(3);
     stop_system_record();
+    
     return 0;
 }
